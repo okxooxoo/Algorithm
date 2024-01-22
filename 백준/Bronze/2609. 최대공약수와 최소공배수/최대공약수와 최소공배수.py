817a953cfb04ccc4eeafcf2e03eafe9de13551d6
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-01-22 17:23:22 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,12 +1,13 @@
+# 유클리드 호제법
 A, B = map(int, input().split())
 
-for i in range(A, 0, -1):
-    if A % i == 0 and B % i == 0:
-        GCF = i # 최대공약수
-        C = A // i # 서로소
-        D = B // i # 서로소
-        break
-LCM = GCF * C * D # 최소공배수 = 최대공약수 * 서로소
+def GCD(A, B):
+    while B > 0:
+        A, B = B, A % B
+    return A
 
-print(GCF)
-print(LCM)
+def LCM(A, B):
+    return A * B // GCD(A, B)
+
+print(GCD(A, B))
+print(LCM(A, B))
