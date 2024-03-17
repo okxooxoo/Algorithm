b4260c424e15f4bc9c293161d8fbbdda75a4a90a
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-03-17 11:16:45 +0900

    [Silver II] Title: 연속합, Time: 88 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,14 +1,7 @@
 n = int(input())
 P = list(map(int, input().split()))
 
-dp = [P[0]] + [0] * (n - 1)
 for i in range(1, n):
-    if dp[i-1] < 0: # 이전의 연속합이 음수면
-        dp[i] = P[i]
-    else: # 이전의 연속합이 양수면
-        if dp[i-1] + P[i] >= 0:
-            dp[i] = dp[i-1] + P[i]
-        else:
-            dp[i] = P[i]
+    P[i] = max(P[i], P[i-1] + P[i])
 
-print(max(dp))
+print(max(P))
