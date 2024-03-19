fba211ed2cb4902fef7353648756ca4222d9c47b
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-03-20 01:09:14 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,7 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        i = command[0]
-        j = command[1]
-        k = command[2]
+        i, j, k = command
         subArray = sorted(array[i-1 : j])
         answer.append(subArray[k-1])
     return answer
