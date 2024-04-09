5136de2bd004d583457b8bc784cd51e71b24ba3e
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-04-10 08:13:06 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,23 +1,12 @@
 function solution(s) {
-    const arr = Array.from(s);
     let answer = '';
-    let isFirst = true;
     
-    arr.forEach((value) => {
-        if (value === ' ') { // 공백이면
-            answer += value;
-            isFirst = true;
-        } else if (!isNaN(value)) { // 숫자면 (공백도 숫자로 취급하므로 조심!)
-            answer += value;
-            isFirst = false;
-        } else if (value === value.toUpperCase()) { // 대문자면
-            answer += isFirst ? value : value.toLowerCase();
-            isFirst = false;
-        } else { // 소문자면
-            answer += isFirst ? value.toUpperCase() : value;
-            isFirst = false;
-        }
-    });
+    for (let i = 0; i < s.length; i++) {
+        if (i === 0 || s[i-1] === ' ')
+            answer += s[i].toUpperCase();
+        else
+            answer += s[i].toLowerCase();
+    }
     
     return answer;
 }
