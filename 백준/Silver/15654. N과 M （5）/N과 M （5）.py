bd44f9c9ea77d296b487cc67f53a172a362834e3
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-04-22 19:12:49 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,19 @@
+# N개의 자연수 중에서 M개를 고른 수열
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort() # 수열은 사전 순으로 증가하는 순서로 출력
+
+visited = []
+def dfs():
+    if len(visited) == M:
+        print(*visited)
+        return
+    
+    for n in numbers:
+        if n not in visited:
+            visited.append(n)
+            dfs()
+            # 백트래킹
+            visited.pop()
+
+dfs()
