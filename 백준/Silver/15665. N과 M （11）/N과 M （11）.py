7794e0eb736c2f3ec037d16d0b41a200cc1cc55e
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-04-22 19:13:54 +0900

    [Silver II] Title: N과 M (11), Time: 732 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,20 @@
+# N개의 자연수 중에서 M개를 고른 수열(N개의 자연수에 같은 수 포함, 중복허용)
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort() # 수열은 사전 순으로 증가하는 순서로 출력
+
+result = []
+def dfs():
+    if len(result) == M:
+        print(*result)
+        return
+    prev = 0
+    for i in range(N):
+        if numbers[i] != prev:
+            prev = numbers[i]
+            result.append(numbers[i])
+            dfs()
+            # 백트래킹
+            result.pop()
+
+dfs()
