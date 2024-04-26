b6c30c5561b8d306a9c7093482e7a0a855562ccd
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-04-26 13:14:05 +0900

    [Silver III] Title: 다음 순열, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -0,0 +1,14 @@
+N = int(input())
+lst = list(map(int, input().split()))
+
+for i in range(N-1, 0, -1):
+    if lst[i] > lst[i-1]:
+        for j in range(N-1, 0, -1):
+            if lst[j] > lst[i-1]:
+                lst[j], lst[i-1] = lst[i-1], lst[j]
+                lst = lst[:i] + sorted(lst[i:])
+                break
+        print(*lst)
+        break
+else:
+    print(-1)
