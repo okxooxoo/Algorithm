532407246aad1146e5751e26d3545cf3cc59b12a
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-05-09 17:10:22 +0900

    [Silver III] Title: 프린터 큐, Time: 76 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -3,17 +3,17 @@ from collections import deque
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
-    dq = deque((map(int, input().split())))
+    lst = list((map(int, input().split())))
     count = 0 # 몇 번째로 인쇄되는지 카운트
     while M >= 0:
-        if dq[0] == max(list(dq)): # 중요도가 가장 높으면
-            dq.popleft()
+        if lst[0] == max(lst): # 중요도가 가장 높으면
+            lst.pop(0)
             M -= 1
             count += 1
         else:
-            dq.append(dq.popleft())
+            lst.append(lst.pop(0))
             if M == 0:
-                M = len(dq) - 1
+                M = len(lst) - 1
             else:
                 M -= 1
     print(count)
