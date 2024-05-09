ae9df00eb8f59cdb91bfe9b7e3bb0410672cd9e7
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-05-09 12:31:57 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,5 +2,5 @@ L = int(input())
 inputs = list(input())
 s = 0
 for i in range(L):
-    s += ((ord(inputs[i]) - 96) * (31 ** i)) % 1234567891
-print(s)
+    s += (ord(inputs[i]) - 96) * (31 ** i)
+print(s % 1234567891)
