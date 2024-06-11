26a6d5185fe2390c827451c13f00b38f4c791360
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-06-11 18:15:38 +0900

    [Silver I] Title: IOIOI, Time: 480 ms, Memory: 43924 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,10 +1,18 @@
 N = int(input())
 M = int(input())
 K = 2 * N + 1
-string = input()
-dest = 'IO' * N + 'I'
-count = 0
-for i in range(M - K + 1):
-    if string[i:i+K] == dest:
-        count += 1
-print(count)
+string = list(input())
+string.reverse()
+sum = 0
+while string:
+    stack = []
+    temp = string.pop()
+    if temp == 'I':
+        stack.append(temp)
+        while string and stack[-1] != string[-1]:
+            stack.append(string.pop())
+        if stack[-1] == 'O':
+            stack.pop()
+        if (len(stack) - K) >= 0:
+            sum += (len(stack) - K) // 2 + 1
+print(sum)
