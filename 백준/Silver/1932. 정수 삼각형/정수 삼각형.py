b79366096e4633c1cf81810a0a399bef02f1bc3f
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-06-13 20:33:33 +0900

    [Silver I] Title: 정수 삼각형, Time: 148 ms, Memory: 35616 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,10 @@
+n = int(input())
+s = 0
+triangle = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    triangle.append(row)
+for i in range(n-2, -1, -1):
+    for j in range(i+1):
+        triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
+print(triangle[0][0])
