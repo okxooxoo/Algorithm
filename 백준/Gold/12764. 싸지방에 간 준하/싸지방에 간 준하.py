8d9e0ef7ea542de4ce9a9c2f116309c7cefff2d2
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-08-01 18:50:41 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 440 ms, Memory: 53864 KB -BaekjoonHub

--- 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
@@ -13,14 +13,9 @@ for _ in range(N):
     schedule.append((start, end))
 
 schedule.sort(key=lambda x: x[0])  # 시작 시각을 기준으로 정렬
-heapq.heappush(min_heap, (schedule[0][1], schedule[0][0], 0))  # 종료 시간, 시작 시간, 컴퓨터 index
-computer_count[0] += 1
 
 count = 0
-for i in range(1, N):
-    start = schedule[i][0]
-    end = schedule[i][1]
-
+for start, end in schedule:
     # 사용 가능한 컴퓨터 갱신
     while min_heap and min_heap[0][0] <= start:
         temp = heapq.heappop(min_heap)
@@ -32,10 +27,10 @@ for i in range(1, N):
         heapq.heappush(min_heap, (end, start, index))
 
     else:
-        count += 1
         computer_count[count] += 1
         heapq.heappush(min_heap, (end, start, count))
+        count += 1
         
 # 출력
-print(count+1)
-print(*computer_count[:count+1])
+print(count)
+print(*computer_count[:count])
