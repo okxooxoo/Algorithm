787eacbb8fc76565ff7655cff2eb9c4fc26285af
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-08-07 00:37:18 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 64 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,27 +1,18 @@
 from collections import deque
 
 N = int(input())
-inputs = list(map(int, input().split()))
-balloon = deque()
-
-for i in range(N):
-    # 풍선 번호와 종이에 적힌 값
-    balloon.append((i+1, inputs[i]))
-
+balloon = deque(enumerate(map(int, input().split())))
 answer = []
 
 while balloon:
     index, delta = balloon.popleft()
-    answer.append(index)
-    if balloon:
-        # 오른쪽으로 이동해야 하면
-        if delta > 0:
-            for _ in range(delta - 1):
-                balloon.append(balloon.popleft())
-        # 왼쪽으로 이동해야 하면
-        else:
-            for _ in range(-delta):
-                balloon.appendleft(balloon.pop())
+    answer.append(index + 1)
+
+    # 오른쪽으로 이동해야 하면
+    if delta > 0:
+        balloon.rotate(-(delta - 1))
+    # 왼쪽으로 이동해야 하면
+    else:
+        balloon.rotate(-delta)
 
 print(*answer)
-    
