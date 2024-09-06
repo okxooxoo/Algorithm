cfeb3f8827f214a4256ac55fd19aba3b0e767b48
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-09-07 03:17:37 +0900

    [Gold V] Title: 센서, Time: 36 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -6,27 +6,13 @@ if K >= N:
 else:
     sensor = list(map(int, input().split())) # 센서 좌표
     distance = [] # 센서 사이의 거리
-    center = [] # 집중국 좌표
 
     sensor.sort() # 센서 좌표 오름차순 정렬
 
     for i in range(N - 1):
         d = sensor[i + 1] - sensor[i]
-        distance.append((d, i))
+        distance.append(d)
 
     distance.sort(reverse=True) # 센서 사이의 거리 내림차순 정렬
 
-    for i in range(K - 1):
-        d, end = distance[i]
-        center.append(end)
-
-    center.sort() # 집중국 좌표 오름차순 정렬
-
-    sum = 0
-    start = 0
-    for end in center:
-        sum += sensor[end] - sensor[start]
-        start = end + 1
-
-    sum += sensor[N - 1] - sensor[start]
-    print(sum)
+    print(sum(distance[K-1:]))
