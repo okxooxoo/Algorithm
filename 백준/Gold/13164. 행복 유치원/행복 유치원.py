008177278e85fb3c511f135ee860ce5f76c2a543
서로 <94723713+okxooxoo@users.noreply.github.com>
2024-09-07 03:14:49 +0900

    [Gold V] Title: 행복 유치원, Time: 196 ms, Memory: 66124 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -5,26 +5,9 @@ height_differ = []
 
 # 이웃한 원생 간 키의 차이 저장
 for i in range(N - 1):
-    height_differ.append((height[i + 1] - height[i], i))
+    height_differ.append(height[i + 1] - height[i])
 
 # 인접한 두 원생의 키 차이가 크면 조를 나누기 위해 내림차순 정렬
 height_differ.sort(reverse=True)
 
-# 해당 조에서 가장 오른쪽에 있는 사람(가장 키 큰 사람)
-end_person = []
-
-# 조를 나눌 위치 저장
-for i in range(K - 1):
-    differ, end = height_differ[i]
-    end_person.append(end)
-
-end_person.sort()
-sum = 0
-start = 0
-for end in end_person:
-    sum += height[end] - height[start]
-    start = end + 1
-
-sum += height[N - 1] - height[start]
-
-print(sum)
+print(sum(height_differ[K-1:]))
