df9f0df5cd35dc386076bcd7617e58fef67f86fa
서로 <94723713+okxooxoo@users.noreply.github.com>
2025-09-11 21:34:46 +0900

    [Silver I] Title: 추월, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.java
+++ 백준/Silver/2002. 추월/추월.java
@@ -7,19 +7,19 @@ public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     private static int N; // 차의 대수
-
-    private static String[] departure;
-    private static HashMap<String, Integer> arrival;
+    private static HashMap<String, Integer> departure;
+    private static String[] arrival;
 
     private static int answer;
 
     private static void solution() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arrival.get(departure[i]) < arrival.get(departure[j])) {
-                    answer++;
-                    break;
-                }
+        int min = departure.get(arrival[N - 1]); // 터널에서 가장 마지막으로 나온 자동차의 터널에 들어간 순서
+
+        for (int i = N - 2; i >= 0; i--) {
+            if (min < departure.get(arrival[i])) {
+                answer++;
+            } else {
+                min = departure.get(arrival[i]);
             }
         }
     }
@@ -28,17 +28,17 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         answer = 0;
 
-        departure = new String[N];
-        arrival = new HashMap<>();
+        departure = new HashMap<>();
+        arrival = new String[N];
 
         for (int i = 0; i < N; i++) {
             String carNumber = br.readLine();
-            departure[i] = carNumber;
+            departure.put(carNumber, i);
         }
 
         for (int i = 0; i < N; i++) {
             String carNumber = br.readLine();
-            arrival.put(carNumber, i);
+            arrival[i] = carNumber;
         }
     }
 
