37e593c1f11c0e65f4ea75db3995f463d3294a43
서로 <94723713+okxooxoo@users.noreply.github.com>
2026-01-01 15:29:22 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,11 +1,11 @@
 class Solution {
-    int MAX_TIME = 24;
+    int MAX_TIMES = 24;
     
     public int solution(int[] players, int m, int k) {
         int count = 0; // 증설 횟수
-        int[] servers = new int[MAX_TIME]; // 현재 증설된 서버 수
+        int[] servers = new int[MAX_TIMES]; // 현재 증설된 서버 수
         
-        for (int i = 0; i < MAX_TIME; i++) {
+        for (int i = 0; i < MAX_TIMES; i++) {
             // 현재 필요한 서버 수
             int required = players[i] / m;
             int realReq = required - servers[i];
@@ -13,7 +13,7 @@ class Solution {
                 count += realReq;
                 
                 for (int j = i; j < i + k; j++) {
-                    if (j >= MAX_TIME) break;
+                    if (j >= MAX_TIMES) break;
                     servers[j] += realReq;
                 }
             }
