095c31e7787e5b6782cae092b8dac490e733e120
서로 <94723713+okxooxoo@users.noreply.github.com>
2026-01-28 23:28:06 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,31 +1,29 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
-        char[] N = br.readLine().toCharArray();
-        int[] count = new int[10];
+        int[] numbers = new int[10];
+        String[] inputs = br.readLine().split("");
 
-        for (char c : N) {
-            count[c - '0']++;
+        for (String input : inputs) {
+            numbers[input.charAt(0) - '0']++;
         }
 
-        int answer = 0;
-
+        int mx = 0;
         for (int i = 0; i < 10; i++) {
-            if (answer < count[i]) {
-                if (i == 6 || i == 9) {
-                    answer = Math.max(answer, ((count[6] + count[9]) + 1) / 2);
-                } else {
-                    answer = count[i];
-                }
-            }
+            if (i == 6 || i == 9) continue;
+            mx = Math.max(mx, numbers[i]);
         }
 
-        bw.write(answer + "\n");
-        bw.flush();
-        bw.close();
+        mx = Math.max(mx, (numbers[6] + numbers[9] + 1) / 2);
+
+        System.out.println(mx);
     }
 }
