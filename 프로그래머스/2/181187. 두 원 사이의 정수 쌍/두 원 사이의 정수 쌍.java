b4c82414c855a38c645ccb7fbd807f4679f3728c
서로 <94723713+okxooxoo@users.noreply.github.com>
2026-03-01 17:01:39 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 20.04 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,15 +1,14 @@
 class Solution {
     public long solution(int r1, int r2) {
         long answer = 0;
- 
-        for (int i=1; i<=r2; i++) {
-            long minJ = (int) Math.ceil(Math.sqrt(1.0*r1*r1 - 1.0*i*i));
-            long maxJ = (int) Math.floor(Math.sqrt(1.0*r2*r2 - 1.0*i*i));
- 
-            answer += (maxJ - minJ + 1);
- 
+        
+        // O(n)
+        for (int x = 1; x <= r2; x++) {
+            long maxY = (long) Math.floor(Math.sqrt(1.0*r2*r2 - 1.0*x*x));
+            long minY = (long) Math.ceil(Math.sqrt(1.0*r1*r1 - 1.0*x*x));
+            answer += maxY - minY + 1;
         }
- 
+        
         return answer * 4;
     }
 }
